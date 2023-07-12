d98f2622811dc64996fb2657faea869eb9020876
Douglas Adams <douglas.adams@me.com>
2023-07-12 13:47:13 -0400

    Initial Commit

---
+++ SDRLog/SDRLogApp.swift
@@ -0,0 +1,17 @@
+//
+//  SDRLogApp.swift
+//  SDRLog
+//
+//  Created by Douglas Adams on 7/12/23.
+//
+
+import SwiftUI
+
+@main
+struct SDRLogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
