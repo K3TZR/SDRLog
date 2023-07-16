2feabeed7868433ad954fb4d7a22602dc690a3e4
Douglas Adams <douglas.adams@me.com>
2023-07-16 12:48:47 -0400

    v1.1.1

--- SDRLog/SDRLogApp.swift
+++ SDRLog/SDRLogApp.swift
@@ -5,13 +5,27 @@
 //  Created by Douglas Adams on 7/12/23.
 //
 
+import ComposableArchitecture
 import SwiftUI
+
+import LogView
+import Shared
 
 @main
 struct SDRLogApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
+  
+  var flexFolderUrl: URL? {
+    let container = FileManager().containerURL(forSecurityApplicationGroupIdentifier: DefaultValues.flexSuite)
+    return container?.appending(path: "Library/Application Support/Logs")
+  }
+  
+  var body: some Scene {
+    WindowGroup {
+      if flexFolderUrl == nil {
+        Text("FATAL ERROR - Flex App Group folder not found")
+      } else {
+        LogView(store: Store(initialState: LogFeature.State(domain: "net.k3tzr", appName: "Sdr6000", folderUrl: flexFolderUrl!), reducer: LogFeature()) )
+      }
     }
+  }
 }
